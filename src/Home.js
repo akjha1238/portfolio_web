7fa174800355d2d6ff058c8be0babbb012439538
akjha1238 <51783680+akjha1238@users.noreply.github.com>
2021-02-03 15:04:22 +0530

    deploy on github

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+class Home extends Component {
+    
+    render() {
+        return (
+            <div>
+           <div>Hii, New Project</div>
+           <div>We updated it</div> 
+           </div>
+        );
+    }
+}
+
+export default Home;
